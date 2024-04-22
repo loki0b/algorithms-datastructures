3ee01b1b67006e2a9d2a26360d4f9d7e506b7a34
loki0b <riqc0nt4ct@hotmail.com>
2024-04-21 21:26:16 -0300

    update bubble sort

--- sort/bubble_sort.c
+++ sort/bubble_sort.c
@@ -4,7 +4,7 @@ void bubble_sort(int *array, int size) {
     int n;
 
     n = size;
-    for (int i = 0; i <= n -2; i++) {
+    for (int i = 0; i <= n - 2; i++) {
         for (int j = 0; j <= n - 2 - i; j++) {
             if (array[j + 1] < array[j]) swap(&array[j], &array[j + 1]);
         }
