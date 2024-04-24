e1760ca755a6196346e51691c130f54f1329d532
loki0b <riqc0nt4ct@hotmail.com>
2024-04-24 09:07:56 -0300

    add main function

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "tools.h"
+#include "sort.h"
+
+int main(void) {
+    int array[] = {3, 2, 1, 7, 4, 42, 13, 78, 5, 23, 111, 9, 8};
+    size_t array_size = sizeof(array) / sizeof(array[0]);
+    
+    for (int i = 0; i < array_size; i++) printf("%d ", array[i]);
+    printf("\n");
+    //selection_sort(array, array_size);
+    //bubble_sort(array, array_size);
+    insertion_sort(array, array_size);
+    for (int i = 0; i < array_size; i++) printf("%d ", array[i]);
+    printf("\n");
+
+    return 0;
+}
