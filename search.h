4ab1fe9f16b80ce476ee51dad3428b44020e2f33
loki0b <riqc0nt4ct@hotmail.com>
2024-04-26 11:04:09 -0300

    add sequential search

---
+++ search.h
@@ -0,0 +1,7 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+int sequential_search(int *array, int k, size_t array_size);
+int sorted_sequential_search(int *array, int k);
+
+#endif
---
+++ search/sequential_search.c
@@ -0,0 +1,31 @@
+// Implements sequential search
+// Method: Brute force
+// Input: An array A of n elements, a search key K and the array size
+// Output: The index of the first element A[0..n - 1] whose value is equal to K or -1 if no such element is found
+
+#include <stddef.h>
+
+// non sorted array sequential search
+int sequential_search(int *array, int k, size_t array_size) {
+    int i, n;
+    
+    n = array_size;
+    i = 0;
+    while (array[i] != k) {
+        i++;
+    }
+    if (i < n) return i;
+    else return -1;
+}
+
+// sorted array sequential search
+int sorted_sequential_search(int *array, int k) {
+    int i;
+
+    i = 0;
+    while (array[i] < k) {
+        i++;
+    }
+    if (array[i] == k) return i;
+    else return -1;
+}
