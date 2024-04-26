2c857ef619a477c2fe8a30bb8f1338339836cbfe
loki0b <riqc0nt4ct@hotmail.com>
2024-04-26 02:47:51 -0300

    fix merge function. r to r + 1 to have temp[n]

--- sort/merge_sort.c
+++ sort/merge_sort.c
@@ -1,10 +1,12 @@
 #include <stddef.h>
+#include <stdlib.h>
 
 void merge(int *array, size_t l, size_t r) {  // l -> the index 0 of this part of this array, r -> the lenght of this part of the array
     size_t m; // the median index of the array
     size_t i1, i2; // auxiliar index;
-    int temp_array[r];
+    int *temp_array;
     
+    temp_array = malloc(sizeof(array[0]) * (r + 1));
     for (int i = l; i <= r; i++) temp_array[i] = array[i]; // copying
 
     m = (l + r) / 2;
@@ -16,6 +18,8 @@ void merge(int *array, size_t l, size_t r) {  // l -> the index 0 of this part o
         else if (temp_array[i1] <= temp_array[i2]) array[curr] = temp_array[i1++];
         else array[curr] = temp_array[i2++];
     }
+
+    free(temp_array);
 }
 
 // Sorts array A[0..n - 1] by recursive mergesort
