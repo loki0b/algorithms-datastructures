9aee94c6d9e36df4da18f2fbda25c1445a66ce5c
loki0b <riqc0nt4ct@hotmail.com>
2024-04-27 00:24:53 -0300

    update and fix quick sort and hoare partition

--- sort/quick_sort.c
+++ sort/quick_sort.c
@@ -1,13 +1,33 @@
+// Sorts a subarray by quicksort
+// Method: Divide-and-Conquer
+// Input: Subarray of array A[0..n - 1], defined by its left and right indices l and r
+// Output: Subarray A[l..r] sorted in nondecreasing order
+
 #include <stddef.h>
 #include "../tools.h"
 
+int hoare_partition(int *array, int l, int r);
 
-size_t hoare_partition(int *array, size_t l, size_t r) {
+void quick_sort(int *array, int l, int r) {
+    if (l < r) {
+        int s; // split position
+
+        s = hoare_partition(array, l, r);
+        quick_sort(array, l, s - 1);
+        quick_sort(array, s + 1, r);
+    }
+}
+
+// Partitions a subarray by Hoare's algorithm, using the first element as a pivot
+// Input: Subarray of array A[0..n - 1], defined by its left and right indices l and r (l < r)
+// Output: Partition of A[a..r], with the split position returned as this function's value
+int hoare_partition(int *array, int l, int r) {
     int p, i, j;
 
     p = array[l];
     i = l;
     j = r + 1;
+
     do {
         
         do {
@@ -25,14 +45,4 @@ size_t hoare_partition(int *array, size_t l, size_t r) {
     swap(&array[l], &array[j]);
 
     return j;
-}
-
-void quick_sort(int *array, size_t l, size_t r) {
-    if (l < r) {
-        size_t s; // split position
-
-        s = hoare_partition(array, l, r);
-        quick_sort(array, l, s - 1);
-        quick_sort(array, s + 1, r);
-    }
 }
