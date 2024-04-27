35745d570e0975aeb0590534f5b2c8d0316640c7
loki0b <riqc0nt4ct@hotmail.com>
2024-04-27 00:25:04 -0300

    update main

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
 // TODO: how to create docs for functions in C?
 
 int main(void) {
-    int array[] = {3, 2, 1, 7, 4, 42, 13, 78, 5, 23, 111, 9, 8};
-    size_t array_size = sizeof(array) / sizeof(array[0]);
+    int array[] = {3, 2, 1, 7, 4, 42, 42, 13, 78, 5, 23, 111, 9, 8};
+    int array_size = sizeof(array) / sizeof(array[0]);
     
     for (int i = 0; i < array_size; i++) printf("%d ", array[i]);
     printf("\n");
@@ -17,8 +17,8 @@ int main(void) {
     //merge_sort(array, 0, array_size - 1); // array_size should be n - 1
     //int index = sorted_sequential_search(array, 7);
     quick_sort(array, 0, array_size - 1);
-    int index = binary_search(array, 42, array_size);
-    printf("index: %d\n", index);
+    //int index = binary_search(array, 42, array_size);
+    //printf("index: %d\n", index);
     for (int i = 0; i < array_size; i++) printf("%d ", array[i]);
     printf("\n");
 
