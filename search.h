4e9f66f02b0335e23c395d18bb0703876b30eed3
loki0b <riqc0nt4ct@hotmail.com>
2024-04-28 16:43:49 -0300

    update type from size_t to int

--- search.h
+++ search.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
-int sequential_search(int *array, int k, size_t array_size);
+int sequential_search(int *array, int k, int array_size);
 int sorted_sequential_search(int *array, int k);
 int binary_search(int *array, int k, int array_size);
 
