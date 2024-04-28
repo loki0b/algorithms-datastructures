17734fb5b754e2878f704dc489ab173c33d48cb7
loki0b <riqc0nt4ct@hotmail.com>
2024-04-28 16:43:26 -0300

    change size_t type to int

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-void selection_sort(int *array, size_t array_size);
-void bubble_sort(int *array, size_t array_size);
-void insertion_sort(int *array, size_t array_size);
+void selection_sort(int *array, int array_size);
+void bubble_sort(int *array, int array_size);
+void insertion_sort(int *array, int array_size);
 void merge_sort(int *array, int l, int r);
 void quick_sort(int *array, int l, int r);
 
