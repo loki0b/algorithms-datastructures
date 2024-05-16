0ed69338d23d63214c510cb3cb1a04380229a3fa
loki0b <riqc0nt4ct@hotmail.com>
2024-05-15 21:43:05 -0300

    fixing array bounds in sequential search

--- search/sequential_search.c
+++ search/sequential_search.c
@@ -4,26 +4,24 @@
 // Output: The index of the first element A[0..n - 1] whose value is equal to K or -1 if no such element is found
 
 // non sorted array sequential search
-int sequential_search(int *array, int k, int array_size) {
-    int i, n;
+int sequential_search(int *array, int k, int n) {
     
-    n = array_size;
-    i = 0;
-    while (array[i] != k) {
-        i++;
+    for (int i = 0; i < n; i++) {
+        if (array[i] == k) { return i; }
     }
-    if (i < n) return i;
-    else return -1;
+
+    return -1;
 }
 
 // sorted array sequential search
-int sorted_sequential_search(int *array, int k) {
+int sorted_sequential_search(int *array, int k, int n) {
     int i;
-
+    
     i = 0;
-    while (array[i] < k) {
+    while (i < n) {
+        if (array[i] >= k) { break; }
         i++;
     }
-    if (array[i] == k) return i;
-    else return -1;
+    if (array[i] == k) { return i; }
+    else { return -1; }
 }
