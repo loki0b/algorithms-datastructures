b90ba1801cc43c0f6974be7e625155313c1ed97e
loki0b <riqc0nt4ct@hotmail.com>
2024-05-23 17:05:54 -0300

    update array list

--- include/array_list.h
+++ include/array_list.h
@@ -2,19 +2,19 @@
 #define ARRAYLIST_H
 
 typedef struct list array_list;
-array_list* create_list(int size);
-int insert(array_list *l, int element);
-int append(array_list *l, int element);
-int pop(array_list *l);
-int clear(array_list *l);
-void move_to_start(array_list *l);
-void move_to_end(array_list *l);
-int move_to_position(array_list *l, int p);
-int previous(array_list *l);
-int next(array_list *l);
-int length(array_list *l);
-int curr_position(array_list *l);
-int get_value(array_list *l, int pos);
-void delete_array_list(array_list *l);
+array_list* al_create_list(int size);
+int al_insert(array_list *l, int element);
+int al_append(array_list *l, int element);
+int al_pop(array_list *l);
+int al_clear(array_list *l);
+void al_move_to_start(array_list *l);
+void al_move_to_end(array_list *l);
+int al_move_to_position(array_list *l, int p);
+int al_previous(array_list *l);
+int al_next(array_list *l);
+int al_length(array_list *l);
+int al_curr_position(array_list *l);
+int al_get_value(array_list *l, int pos);
+void al_delete_list(array_list *l);
 
 #endif
--- list/array_list.c
+++ list/array_list.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-typedef struct {
+typedef struct list {
     int max_size; // capacity
     int list_size; // number of elements
     int curr; // position of the cursor
@@ -8,7 +8,7 @@ typedef struct {
 } array_list;
 
 // create a new array_list
-array_list* create_list(int size) {
+array_list* al_create_list(int size) {
     array_list *l = (array_list*) malloc(sizeof (array_list));
     if (l == NULL) { 
         return NULL; 
@@ -22,7 +22,7 @@ array_list* create_list(int size) {
 }
 
 // inserts a new element in curr postion in the array list
-int insert(array_list *l, int element) {
+int al_insert(array_list *l, int element) {
     if (l->list_size >= l->max_size) {
         return -1; 
     }
@@ -41,7 +41,7 @@ int insert(array_list *l, int element) {
 }
 
 // insert a new element in the final of array list
-int append(array_list *l, int element) {
+int al_append(array_list *l, int element) {
     if (l->list_size >= l->max_size) { return -1; } // error
     
     l->array_list[l->list_size] = element;
@@ -51,7 +51,7 @@ int append(array_list *l, int element) {
 }
 
 // remove an element in the curr position, then return it
-int pop(array_list *l) {
+int al_pop(array_list *l) {
     if (l->list_size == 0) {
         return -1;
     }
@@ -70,21 +70,21 @@ int pop(array_list *l) {
 }
 
 // clear the array list
-void clear(array_list *l) {
+void al_clear(array_list *l) {
     l->list_size = 0;
     l->curr = 0;
 }
 
 // move the curr
-void move_to_start(array_list *l) {
+void al_move_to_start(array_list *l) {
     l->curr = 0;
 }
 
-void move_to_end(array_list *l) {
+void al_move_to_end(array_list *l) {
     l->curr = l->list_size;
 }
 
-int move_to_position(array_list *l, int p) {
+int al_move_to_position(array_list *l, int p) {
     if (p < 0 || p > l->list_size) {
         return -1;
     }
@@ -94,7 +94,7 @@ int move_to_position(array_list *l, int p) {
     return 0;
 }
 
-int previous(array_list *l) {
+int al_previous(array_list *l) {
     if (l->curr == 0) {
         return -1;
     }
@@ -104,7 +104,7 @@ int previous(array_list *l) {
     return 0;
 }
 
-int next(array_list *l) {
+int al_next(array_list *l) {
     if (l->curr >= l->list_size) {
         return -1;
     }
@@ -114,15 +114,15 @@ int next(array_list *l) {
     return 0;
 }
 
-int length(array_list *l) { 
+int al_length(array_list *l) { 
     return l->list_size;
 }
 
-int curr_position(array_list *l) {
+int al_curr_position(array_list *l) {
     return l->curr;
 }
 
-int get_value(array_list *l) {
+int al_get_value(array_list *l) {
     if (l->list_size == 0) {
         return -1;
     }
@@ -131,6 +131,6 @@ int get_value(array_list *l) {
 }
 
 // free the array of the list struct
-void delete_array_list(array_list *l) {
+void al_delete_list(array_list *l) {
     free(l->array_list);
 }
