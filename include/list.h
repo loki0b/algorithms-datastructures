600c1634d04d0940c2fe67adc15febd0d7a8eb1f
loki0b <riqc0nt4ct@hotmail.com>
2024-05-27 20:37:22 -0300

    remove headers

--- include/list.h
+++
@@ -1,3 +0,0 @@
-#include "array_list.h"
-#include "linked_list.h"
-#include "stack.h"
--- include/search.h
+++
@@ -1,8 +0,0 @@
-#ifndef SEARCH_H
-#define SEARCH_H
-
-int sequential_search(int *array, int k, int n);
-int sorted_sequential_search(int *array, int k, int n);
-int binary_search(int *array, int k, int n);
-
-#endif
