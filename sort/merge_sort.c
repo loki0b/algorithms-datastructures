f0062d77d57623b5bebb22b1c9a296f8246ed743
loki0b <riqc0nt4ct@hotmail.com>
2024-06-02 21:14:58 -0300

    improving merge sort

--- sort/merge_sort.c
+++ sort/merge_sort.c
@@ -3,14 +3,11 @@
 // Input: An array A[0..n - 1] of orderable elements
 // Output: An array A[0..n - 1] sorted in nondecreasing order
 
-#include <stdlib.h>
-
 void merge(int *array, int l, int r, int n) {  // l -> the index 0 of this part of this array, r -> the lenght of this part of the array
     int m; // the median index of the array
     int i1, i2; // auxiliar index;
-    int *temp_array;
+    int temp_array[n];
     
-    temp_array = malloc(sizeof (int) * n);
     for (int i = l; i <= r; i++) temp_array[i] = array[i]; // copying
 
     m = (l + r) / 2;
@@ -22,8 +19,6 @@ void merge(int *array, int l, int r, int n) {  // l -> the index 0 of this part 
         else if (temp_array[i1] <= temp_array[i2]) array[curr] = temp_array[i1++];
         else array[curr] = temp_array[i2++];
     }
-
-    free(temp_array);
 }
 
 void merge_sort(int *array, int l, int r, int n) { // l -> the leftmost index of the array, r -> the rightmost index of the array
